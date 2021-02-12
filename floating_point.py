a5867c991dbd05245b2ee215cdb2042d561f24a4
Elijah J. Passmore <passmoreelijahj@elijahjpassmore.com>
2021-02-12 21:09:02 +1300

    Move exception handling to within floating_point
    - Move exception handling to within floating_point to make it more
      flexible.
    - Add new exceptions.
    
    Signed-off-by: Elijah J. Passmore <passmoreelijahj@elijahjpassmore.com>

--- floating_point.py
+++ floating_point.py
@@ -2,13 +2,61 @@ import os
 import sys
 
 
-def floating_point(in_file, in_precision, out_file, out_precision):
-    raise NotImplementedError
+class InvalidFile(OSError):
+    """Raised if the path or permissions of a provided file is
+    invalid.
+    """
+
+    def __init__(self, path,
+                 message="Cannot open file."):
+        self.path = path
+        self.message = message
+
+    def __str__(self):
+        return (f"{self.path}: {self.message}")
+
+
+class InvalidPrecision(ValueError):
+    """Raised if precision is not either single or double."""
+
+    def __init__(self, precision,
+                 message="Precision must be either single or double."):
+        self.precision = precision
+        self.message = message
+
+    def __str__(self):
+        return (f"{self.precision}: {self.message}")
+
+
+def floating_point(in_path, in_precision, out_path, out_precision):
+    in_file, out_file = None, None
+
+    # Attempt to open input file at in_path with read permission.
+    try:
+        in_file = open(in_path, "r")
+    except PermissionError:
+        raise InvalidFile(
+            in_path, "Insufficient permission to read file.")
+    except OSError:
+        raise InvalidFile(in_file)
+
+    # Attempt to open output file at out_path with write permission.
+    try:
+        out_file = open(out_path, "w")
+    except PermissionError:
+        raise InvalidFile(
+            out_path, "Insufficient permission to write to file.")
+    except OSError:
+        raise InvalidFile(out_file)
+
+    # Validate in_precision and out_precision.
+    if (in_precision != "single") and (in_precision != "double"):
+        raise InvalidPrecision(in_precision)
+    elif (out_precision != "single") and (out_precision != "double"):
+        raise InvalidPrecision(out_precision)
 
 
 if __name__ == "__main__":
-    # File objects.
-    in_file, out_file = None, None
     # File paths in string form.
     in_path, out_path = "", ""
     # Precision of each file.
@@ -39,18 +87,7 @@ if __name__ == "__main__":
     except EOFError:
         sys.exit()
 
-    # Attempt to open input file at in_path with read permission.
     try:
-        in_file = open(in_path, "r")
-    except:
-        print("Unable to read file: " + in_path)
-        sys.exit()
-
-    # Attempt to open output file at out_path with write permission.
-    try:
-        out_file = open(out_path, "w")
-    except:
-        print("Unable to write to file: " + out_path)
-        sys.exit()
-
-    floating_point(in_file, in_precision, out_file, out_precision)
+        floating_point(in_path, in_precision, out_path, out_precision)
+    except Exception as e:
+        print(e)
